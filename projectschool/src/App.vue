c32b9b66b5c6426356a7290165c112c3ac333cee
Marco <marcoviniciusmoreira@live.com>
2019-07-20 00:25:32 -0300

    CRUD

--- projectschool/src/App.vue
+++ projectschool/src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Marco Vinicius" />
+    <Alunos />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Alunos from "./components/Aluno/Alunos.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Alunos
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* #app {
+} */
+
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
+body {
+  background-color: #eee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  justify-items: center;
+}
+
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
+
+table {
+  margin: 0px;
+  padding: 0px;
+  list-style-type: none;
+  width: 100%;
+}
+
+table tr td {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+table thead th {
+  background-color: rgb(184, 208, 216) !important;
+  font-size: 1.2em;
+  padding: 10px 0px;
+  text-align: center !important;
+}
+
+.colPequeno {
+  width: 5%;
+}
+
+.btnDanger {
+  background-color: #fa4430;
+}
+
+.btn {
+  border: 1px solid black;
+  padding: 10px 20px;
+  cursor: pointer;
+  color: white;
+  font-weight: bold;
+  border-radius: 5px;
+  border-bottom: 3px solid black;
+}
+
+.btn:hover {
+  text-shadow: 1px 1px 1px black;
+  border-bottom: 1px solid black;
+  margin-top: 3px;
 }
 </style>
---
+++ projectschool/src/components/Aluno/Alunos.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <titulo texto="Aluno" />
+
+    <div>
+      <input type="text" placeholder="Nome do Aluno" v-model="nome" @keyup.enter="adicionar()" />
+      <button class="btn btnInput" @click="adicionar()">adicionar</button>
+    </div>
+
+    <table>
+      <thead>
+        <th>Matricula</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{aluno.id}}</td>
+          <td>{{aluno.nome}} {{aluno.sobrenome}}</td>
+          <td>
+            <button class="btn btnDanger" @click="remover(aluno)">remover</button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>Nenhum aluno encontrado</tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from "../_shared/Titulo";
+
+export default {
+  components: {
+    Titulo
+  },
+  data() {
+    return {
+      titulo: "Alunos",
+      nome: "",
+      alunos: []
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/alunos")
+      .then(res => res.json())
+      .then(alunos => (this.alunos = alunos));
+  },
+  props: {},
+  methods: {
+    adicionar() {
+      let _aluno = {
+        nome: this.nome,
+        sobrenome: ""
+      };
+
+      this.$http
+        .post("http://localhost:3000/alunos", _aluno)
+        .then(res => res.json())
+        .then(aluno => {
+          this.alunos.push(aluno);
+          this.nome = "";
+        });
+    },
+    remover(aluno) {
+      this.$http.delete(`http://localhost:3000/alunos/${aluno.id}`).then(() => {
+        let _index = this.alunos.indexOf(aluno);
+        this.alunos.splice(_index, 1);
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  color: #687f7f;
+  display: inline;
+}
+
+.btnInput {
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: rgb(116, 115, 115);
+  display: inline;
+}
+
+.btnInput:hover {
+  padding: 20px;
+  margin: 0px;
+  border: 0;
+}
+</style>
